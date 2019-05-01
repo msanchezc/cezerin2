6d7bf4e54b4b28650249e1cd0ab00244f3d783e5
Manuel Sanchez <manuel@Manuels-iMac.local>
2019-05-01 13:52:00 -0400

    Change MongoDB connection scheme to mongodb+srv

--- config/server.js
+++ config/server.js
@@ -1,6 +1,5 @@
 // config used by server side only
 const dbHost = process.env.DB_HOST || '127.0.0.1';
-const dbPort = process.env.DB_PORT || 27017;
 const dbName = process.env.DB_NAME || 'shop';
 const dbUser = process.env.DB_USER || '';
 const dbPass = process.env.DB_PASS || '';
@@ -8,7 +7,7 @@ const dbCred =
 	dbUser.length > 0 || dbPass.length > 0 ? `${dbUser}:${dbPass}@` : '';
 
 const dbUrl =
-	process.env.DB_URL || `mongodb://${dbCred}${dbHost}:${dbPort}/${dbName}`;
+	process.env.DB_URL || `mongodb+srv://${dbCred}${dbHost}/${dbName}`;
 
 module.exports = {
 	// used by Store (server side)
